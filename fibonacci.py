2ce2c5dec2ff8b61f2bb395562604400aa9c0312
Denis Karanja <deebeat@students.uonbi.ac.ke>
2014-11-19 21:18:16 +0300

    Added Even number checker method

--- fibonacci.py
+++ fibonacci.py
@@ -1,6 +1,21 @@
 def fibonacci(number):
+	total = 0
+
 	if number == 1 or number == 0:
 		return 1
 	else:
+		result = fibonacci(number - 1) + fibonacci(number - 2)
 		
-	
+		if result % 2 == 0:
+			total = total + result
+		print "\n-------", result
+		return result
+
+def evenTerm(number):
+	if number % 2 == 0:
+		return number
+
+
+user = int(raw_input("Enter a number to get it's fibonacci sequence\n"))
+print "Fibonacci %d is %d" % (user, fibonacci(user))
+
